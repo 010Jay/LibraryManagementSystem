22393d8b9eaa64e22d8165398ab735c4179a7dfa
Jason <jsnjaftha3@gmail.com>
2023-04-20 14:56:38 +0200

    Completed IssueRepository and IssueRepositoryTest

--- src/main/java/org/jay010/repository/impl/IssueRepository.java
+++ src/main/java/org/jay010/repository/impl/IssueRepository.java
@@ -7,6 +7,7 @@ import org.jay010.repository.IIssue;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class IssueRepository implements IIssue {
@@ -91,16 +92,102 @@ public class IssueRepository implements IIssue {
 
     @Override
     public Issue update(Issue issue) {
-        return null;
+        db.openConnection();
+        int id = issue.getIssueID();
+
+        try{
+            statement = db.connect.prepareStatement(sqlUpdate + id);
+
+            statement.setInt(1, issue.getUserID());
+            statement.setInt(2, issue.getBookID());
+            statement.setDate(3, new java.sql.Date(issue.getIssueDate().getTime()));
+            statement.setInt(4, issue.getPeriod());
+            statement.setDate(5,  new java.sql.Date(issue.getReturnDate().getTime()));
+            statement.setDouble(6, issue.getFine());
+
+            statement.executeUpdate();
+
+        } catch (SQLException exception) {
+
+            System.out.println("Error: " + exception.getMessage());
+        } finally {
+
+            try {
+                if(statement != null)
+                    statement.close();
+
+                if(db.connect != null)
+                    db.closeConnection();
+
+            } catch (SQLException exception) {
+
+                System.out.println("Error: " + exception.getMessage());
+            }
+
+        }
+
+        return issue;
     }
 
     @Override
-    public boolean delete(Integer integer) {
+    public boolean delete(Integer id) {
+        db.openConnection();
+
+        try{
+            statement = db.connect.prepareStatement(sqlDelete + id);
+            int check = statement.executeUpdate();
+
+            if(check > 0)
+                return true;
+            else
+                return false;
+
+        } catch (SQLException exception) {
+
+            System.out.println("Error: " + exception.getMessage());
+        }
+
         return false;
     }
 
     @Override
     public List<Issue> getAll() {
-        return null;
+        db.openConnection();
+        List<Issue> issueList = new ArrayList<>();
+
+        try{
+            statement = db.connect.prepareStatement(sqlReadAll);
+            result = statement.executeQuery();
+
+            int i = 0;
+            while(result.next()) {
+                Issue issue = IssueFactory.createIssue(result.getInt(1), result.getInt(2), result.getInt(3),
+                        result.getDate(4), result.getInt(5), result.getDate(6),
+                        result.getDouble(7));
+                issueList.add(issue);
+
+                i++;
+            }
+
+        } catch (SQLException exception) {
+
+            System.out.println("Error: " + exception.getMessage());
+        } finally {
+            try{
+                if(result != null)
+                    result.close();
+
+                if(statement != null)
+                    statement.close();
+
+                if(db.connect != null)
+                    db.closeConnection();
+
+            } catch (SQLException exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+
+        return issueList;
     }
 }
--- src/test/java/org/jay010/repository/impl/IssueRepositoryTest.java
+++ src/test/java/org/jay010/repository/impl/IssueRepositoryTest.java
@@ -1,7 +1,6 @@
 package org.jay010.repository.impl;
 
 import org.jay010.entity.Issue;
-import org.jay010.entity.User;
 import org.jay010.factory.IssueFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,22 +21,28 @@ class IssueRepositoryTest {
     @BeforeEach
     void setUp() throws ParseException {
         String stringDate1_1 = "28/03/2023";
-        java.util.Date date1_1 = new SimpleDateFormat("dd/MM/yyyy").parse(stringDate1_1);
+        Date date1_1 = new SimpleDateFormat("dd/MM/yyyy").parse(stringDate1_1);
 
         String stringDate1_2 = "10/04/2023";
         Date date1_2 = new SimpleDateFormat("dd/MM/yyyy").parse(stringDate1_2);
 
         issue1 = IssueFactory.createIssue(0,1006,7, date1_1, 10, date1_2, 0);
 
-        String stringDate2_1 = "15/04/2023";
+        String stringDate2_1 = "5/04/2023";
         Date date2_1 = new SimpleDateFormat("dd/MM/yyyy").parse(stringDate2_1);
 
-        issue2 = IssueFactory.createIssue(0,10,154, date2_1, 5, null, 0);
+        String stringDate2_2 = "10/04/2023";
+        Date date2_2 = new SimpleDateFormat("dd/MM/yyyy").parse(stringDate2_2);
+
+        issue2 = IssueFactory.createIssue(1,1001,8, date2_1, 5, date2_2, 0);
 
         String stringDate3_1 = "28/03/2023";
         Date date3_1 = new SimpleDateFormat("dd/MM/yyyy").parse(stringDate3_1);
 
-        issue3 = IssueFactory.createIssue(0,11,200, date3_1, 10, null, 0);
+        String stringDate3_2 = "10/04/2023";
+        Date date3_2 = new SimpleDateFormat("dd/MM/yyyy").parse(stringDate3_2);
+
+        issue3 = IssueFactory.createIssue(0,11,200, date3_1, 10, date3_2, 0);
     }
 
     @Test
@@ -51,5 +57,31 @@ class IssueRepositoryTest {
         Issue test = issueRepo.read(2);
         System.out.println(test.toString());
         assertNotNull(test);
+    }
+
+    @Test
+    void c_testUpdateIssue() {
+        Issue test = issueRepo.update(issue2);
+        assertNotNull(test);
+    }
+
+    @Test
+    void e_testDeleteIssue() {
+        boolean result = issueRepo.delete(1);
+        assertEquals(result, true);
+    }
+
+    @Test
+    void d_testReadAllIssues() {
+        issueRepo.create(issue1);
+        issueRepo.create(issue2);
+        issueRepo.create(issue3);
+
+        List<Issue> issue = issueRepo.getAll();
+
+        for(Issue issueList : issue) {
+            System.out.println(issueList.toString());
+        }
+
     }
 }
