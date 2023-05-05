0f451c19d5daf18710f518a8c3aa404e133e25b7
Jay010 <jsnjaftha3@gmail.com>
2023-05-05 23:15:04 +0200

    Fix issue regarding Cors

--- src/main/java/org/jay010/App.java
+++ src/main/java/org/jay010/App.java
@@ -2,6 +2,9 @@ package org.jay010;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class App {
@@ -10,4 +13,16 @@ public class App {
         //Run spring application
             SpringApplication.run(App.class, args);
     }
+
+    //Fix issue regarding Cors | (Two applications communicating via rest api from different domains/ports)
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200/");
+            }
+        };
+    }
+    
 }
